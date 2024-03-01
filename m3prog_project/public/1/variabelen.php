782e271a9c77a49389cec10d0aa0c34e88faa4e1
ahmad <37993@ma-web.nl>
2024-03-01 23:53:18 +0100

    les1complete

---
+++ m3prog_project/public/1/variabelen.php
@@ -0,0 +1,9 @@
+<?php
+$naam = ' Ahmad ';
+$woonplek = ' Castricum ';
+$weer = ' 7°C ';
+$brr = ' brrr t is koud ';
+
+echo "Mijn naam is" . $naam . "en ik woon in" . $woonplek;
+
+echo "het weer is erg koud want het is" . $weer  ."buiten" . $brr;
